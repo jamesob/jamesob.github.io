19a8852d94c0169ac6c3368eb5a7fd69cd4f3287
James O'Beirne <james.obeirne@gmail.com>
2017-01-28 11:19:05 -0800

    posts.sh improvement

--- post.sh
+++ post.sh
@@ -3,6 +3,12 @@
 DATE=`date +%Y-%m-%d`
 FILENAME="_posts/${DATE}-${1}.md"
 
+if [ -e "$FILENAME" ]; then
+  echo "File ${FILENAME} already exists"
+  exit 1
+fi;
+
+
 cat <<EOL > ${FILENAME}
 ---
 title: 
